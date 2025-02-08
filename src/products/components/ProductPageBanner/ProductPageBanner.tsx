087e026c267e8e802c67f780c4c51ab80739bd9e
Michael Florendo <michael.florendo.002@gmail.com>
2025-02-08 18:41:50 +0800

    add banner for product page

---
+++ src/products/components/ProductPageBanner/ProductPageBanner.tsx
@@ -0,0 +1,9 @@
+import "./ProductPageBanner.scss";
+
+export default function ProductPageBanner() {
+	return (
+		<div className="banner-container">
+			<img className="banner-img" src="/red shoe sale.png" alt="banner" />
+		</div>
+	);
+}
