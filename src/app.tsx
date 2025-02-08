15e9d21b08e144b2a594bf5b9647eefa756894dd
Michael Florendo <michael.florendo.002@gmail.com>
2025-02-08 18:43:22 +0800

    integrate components for product page

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,14 @@
 import './app.scss'
+import ProductPageBanner from "./products/components/ProductPageBanner/ProductPageBanner";
 import ProductPage from './products/pages/ProductPage/ProductPage';
 import HeaderNav from './shared/components/HeaderNav/HeaderNav';
 
 export function App() {
-  return (<div>
-    <HeaderNav></HeaderNav>
-    <ProductPage></ProductPage>
-  </div>);
+  return (
+		<div>
+			<HeaderNav></HeaderNav>
+			<ProductPageBanner></ProductPageBanner>
+			<ProductPage></ProductPage>
+		</div>
+	);
 }
