b21867d9c5005f3c8eba812a3e950fe57c3869bc
Michael Florendo <michael.florendo.002@gmail.com>
2025-02-16 19:32:45 +0800

    modified header nav to use icons

--- src/shared/components/HeaderNav/HeaderNav.tsx
+++ src/shared/components/HeaderNav/HeaderNav.tsx
@@ -1,18 +1,25 @@
 import './HeaderNav.scss';
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
+import AccountCircleOutlined from "@mui/icons-material/AccountCircleOutlined";
 
 export default function HeaderNav() {
-    return (
-        <header>
-            <div className='logo-container'>Emporium</div>
-            <div className='nav-container'>
-                <ul>
-                    <li>about</li>
-                    <li>search</li>
-                    <li>cart</li>
-                    <li>login</li>
-                </ul>
-            </div>
-        </header>
-
-    );
+	return (
+		<header>
+			<div className="logo-container">Emporium</div>
+			<div className="nav-container">
+				<ul>
+					<li>
+						<SearchOutlined></SearchOutlined>
+					</li>
+					<li>
+						<ShoppingCartOutlined></ShoppingCartOutlined>
+					</li>
+					<li>
+						<AccountCircleOutlined></AccountCircleOutlined>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
 }
