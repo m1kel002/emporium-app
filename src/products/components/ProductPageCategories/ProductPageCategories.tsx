35be82642242e09ed0c881706e62367521dc8f24
Michael Florendo <michael.florendo.002@gmail.com>
2025-02-16 19:33:19 +0800

    modified product page categories to use categories data

--- src/products/components/ProductPageCategories/ProductPageCategories.tsx
+++ src/products/components/ProductPageCategories/ProductPageCategories.tsx
@@ -1,28 +1,22 @@
-import React, { FC } from 'react';
-import './ProductPageCategories.scss';
+import { useState } from "react";
+import "./ProductPageCategories.scss";
 import CategoryCard from "../CategoryCard/CategoryCard";
+import categoriesData from "../../../shared/data/categories.json";
+import { Category } from "../../../shared/models/category";
 
-interface ProductPageCategoriesProps {}
+interface ProductPageCategories {}
 
-const categories = [
-	{ name: "Supermarket", image: "/product1.jpg" },
-	{ name: "Shoes", image: "/default_image.png" },
-	{ name: "Laptops & Computers", image: "/default_image.png" },
-	{ name: "Home Appliances", image: "/default_image.png" },
-	{ name: "Cameras", image: "/default_image.png" },
-	{ name: "Health & Personal Care", image: "/default_image.png" },
-	{ name: "Home Entertainment", image: "/default_image.png" },
-];
-
-const ProductPageCategories: FC<ProductPageCategoriesProps> = () => (
-	<div className="ProductPageCategories" data-testid="ProductPageCategories">
-		<h4>Categories</h4>
-		<div className="CategoriesContainer">
-			{categories.map((category, index) => (
-				<CategoryCard key={index} category={category}></CategoryCard>
-			))}
+export default function ProductPageCategories() {
+	const [categories, setCategories] = useState<Array<Category>>();
+	setCategories(categoriesData);
+	return (
+		<div className="ProductPageCategories" data-testid="ProductPageCategories">
+			<h4>Categories</h4>
+			<div className="CategoriesContainer">
+				{categories?.map((category, index) => (
+					<CategoryCard key={index} category={category}></CategoryCard>
+				))}
+			</div>
 		</div>
-	</div>
-);
-
-export default ProductPageCategories;
+	);
+}
