da29723cecfadbb2f00cbc8a61f4dabed064e5db
Michael Florendo <michael.florendo.002@gmail.com>
2025-04-24 07:40:29 +0800

    add router

---
+++ src/app-routing.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import ProductDetailsPage from "./products/pages/ProductDetailsPage/ProductDetailsPage";
+import ProductPage from "./products/pages/ProductPage/ProductPage";
+
+export const router = createBrowserRouter([
+    {
+		path: '/product/:id', element: <ProductDetailsPage />
+	},
+	{
+		path: '/', element: <ProductPage/>
+	},
+	{
+		path: '*', element: <ProductPage/>
+	}
+])
--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,16 @@
+import { RouterProvider } from 'react-router-dom';
 import './app.scss'
 import ProductPageBanner from "./products/components/ProductPageBanner/ProductPageBanner";
 import ProductPageCategories from "./products/components/ProductPageCategories/ProductPageCategories";
 import ProductPage from './products/pages/ProductPage/ProductPage';
 import HeaderNav from './shared/components/HeaderNav/HeaderNav';
+import {router} from './app-routing';
 
 export function App() {
   return (
 		<div>
 			<HeaderNav></HeaderNav>
+			<RouterProvider router={router} />
 			<ProductPageBanner></ProductPageBanner>
 			<ProductPageCategories></ProductPageCategories>
 			<div className="ProductPageContainer">
