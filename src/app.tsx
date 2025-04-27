c708f826d70f505d79ad0cbf2af9c5a6be4e2f77
Michael Florendo <michael.florendo.002@gmail.com>
2025-04-27 21:18:24 +0800

    define theme primary and secondary color

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,27 @@
-import { RouterProvider } from 'react-router-dom';
-import './app.scss'
-import HeaderNav from './shared/components/HeaderNav/HeaderNav';
-import {router} from './app-routing';
+import { RouterProvider } from "react-router-dom";
+import "./app.scss";
+import HeaderNav from "./shared/components/HeaderNav/HeaderNav";
+import { router } from "./app-routing";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000000",
+    },
+    secondary: {
+      main: "#ffffff",
+    },
+  },
+});
 
 export function App() {
   return (
-		<div>
-			<HeaderNav></HeaderNav>
-			<RouterProvider router={router} />
-		</div>
-	);
+    <div>
+      <ThemeProvider theme={theme}>
+        <HeaderNav></HeaderNav>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </div>
+  );
 }
