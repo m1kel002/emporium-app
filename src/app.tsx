a14623341bfe1172fd6591d0ea7afd8baa4dea98
Michael Florendo <michael.florendo.002@gmail.com>
2025-04-28 21:01:16 +0800

    move theme to theme.ts file

--- src/app.tsx
+++ src/app.tsx
@@ -2,18 +2,9 @@ import { RouterProvider } from "react-router-dom";
 import "./app.scss";
 import HeaderNav from "./shared/components/HeaderNav/HeaderNav";
 import { router } from "./app-routing";
-import { createTheme, ThemeProvider } from "@mui/material";
+import {ThemeProvider } from "@mui/material";
+import theme from "./theme"
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#000000",
-    },
-    secondary: {
-      main: "#ffffff",
-    },
-  },
-});
 
 export function App() {
   return (
