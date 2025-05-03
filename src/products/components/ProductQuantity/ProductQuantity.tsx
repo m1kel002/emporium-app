229caa6d5e2d476a1acb80b56c0434b55f944d95
Michael Florendo <michael.florendo.002@gmail.com>
2025-05-03 20:11:23 +0800

    implement quantity component

--- src/products/components/ProductQuantity/ProductQuantity.tsx
+++ src/products/components/ProductQuantity/ProductQuantity.tsx
@@ -1,18 +1,40 @@
-import './ProductQuantity.scss'
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-const ProductQuantity = () => {
-    return (
-        <div className="product-quantity-container">
-            <button className="minus">
-                <RemoveIcon></RemoveIcon>
-            </button>
-            <span className="quantity">12</span>
-            <button className="add">
-                <AddIcon></AddIcon>
-            </button>
-        </div>
-    );
+import "./ProductQuantity.scss";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+
+interface QuantityProps {
+  quantity: number;
+  setQuantity: React.Dispatch<React.SetStateAction<number>>;
 }
+
+const ProductQuantity = ({ quantity, setQuantity }: QuantityProps) => {
+  const handleQuantity = (value: number) => {
+    if (value < 0 && quantity < 2) {
+      return;
+    }
+
+    setQuantity(quantity + value);
+  };
+
+  return (
+    <div className="product-quantity-container">
+      <button className="minus">
+        <RemoveIcon
+          onClick={() => {
+            handleQuantity(-1);
+          }}
+        ></RemoveIcon>
+      </button>
+      <span className="quantity">{quantity}</span>
+      <button className="add">
+        <AddIcon
+          onClick={() => {
+            handleQuantity(1);
+          }}
+        ></AddIcon>
+      </button>
+    </div>
+  );
+};
 
 export default ProductQuantity;
